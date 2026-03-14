e21f926182b888e9d99eaa4c68525d128ae4bc80
Emanuele Zignale <zignale92@gmail.com>
2026-03-14 15:21:30 +0100

    odoo_17-18-19_translate__file_po
    Odoo_17-18-19_translate_file_po

---
+++ README.md
@@ -0,0 +1,37 @@
+## Branch
+- main-> odoo 18
+- 17 -> odoo 17
+- 19 -> odoo 19
+
+### Tutorial
+- copiare lo script translate_module.py nella cartella  ./odoo-folder/src18/odoo[versione]/
+- Eseguire il comando:
+
+## bash
+'''
+ python3 translate_module.py -d lab18 -m academy_system -p ../lab18 --db_port 5423 --db_user odoo --db_password myodoo 
+'''
+
+### Cosa fa questo comando:
+
+- python3 translate_module.py: lancia lo script nella cartella attuale.
+- -p ../lab[versione]: dice a Odoo di "uscire" dalla cartella attuale e cercare i moduli dentro la cartella lab18.
+- -m [modulo_desiderato]: cerca la cartella [modulo_desiderato] dentro lab[versione].
+- Risultato: lo script creerà il file in esempio: ~/odoo-folder/src18/lab18/[modulo_desiderato]/i18n/it_IT.po.
+
+----------------------------------------------------------------------
+
+In alternativa mettere nel workspace quello che ce scritto in workspace.code-workspace il file
+lo si trova nella repository basta copiare e incollare e corregere i percorsi delle cartella 
+
+--------------------------------------------------------------------------------------
+
+## Nota: 
+- Il modulo a cui si deve esportare la traduzione deve essere installato su odoo se no il file .po sarà vuoto
+- Se la lingua del modulo in odoo non e installata lo script dara errore
+Esempio : se il modulo ha la lingua italiana  e in odoo non e installata la lingua installata , lo script dara errore
+- Le traduzioni messe manualmente da inetrfaccia odoo li prende a condizione che la lingua della traduzione sia installata su odoo stesso
+e i campi abbiano  "translate=True"
+# Questo SÌ - la traduzione manuale viene salvata e esportata
+name = fields.Char(string='Nome', translate=True)
+
---
+++ translate_module.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+import odoo
+from odoo.tools.translate import trans_export
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--db', required=True)
+parser.add_argument('-m', '--module', required=True)
+parser.add_argument('-p', '--path', required=True)
+parser.add_argument('-l', '--lang', default='it_IT')
+parser.add_argument('--db_port', default='5432')
+parser.add_argument('--db_user', default='odoo')
+parser.add_argument('--db_password', default='')
+args = parser.parse_args()
+
+addons_path = os.path.abspath(args.path)
+
+try:
+    odoo.tools.config.parser([
+        '--addons-path=' + addons_path,
+        '--db_host=' + '127.0.0.1'
+        '--db_port=' + args.db_port,
+        '--db_user=' + args.db_user,
+        '--db_password=' + args.db_password,
+    ])
+except TypeError:
+    odoo.tools.config['addons-path'] = addons_path
+    odoo.tools.config['db_host'] = '127.0.0.1'
+    odoo.tools.config['db_port'] = args.db_port
+    odoo.tools.config['db_user'] = args.db_user
+    odoo.tools.config['db_password'] = args.db_password
+
+registry = odoo.modules.registry.Registry.new(args.db)
+    
+i18n_path = os.path.join(addons_path, args.module, 'i18n')
+    
+if not os.path.exists(i18n_path):
+    os.makedirs(i18n_path, exist_ok=True)
+    print(f"Cartella creata: {i18n_path}")
+
+if args.lang == 'it_IT':
+    filename = os.path.join(i18n_path, f"{args.module}.po")
+    export_lang = 'it_IT'     
+    export_format = 'po'
+elif args.lang == 'en_US':
+    filename = os.path.join(i18n_path, f"{args.module}.pot")
+    export_lang = False    
+    export_format = 'pot'
+else:
+    filename = os.path.join(i18n_path, f"{args.lang}.po")
+    export_lang = args.lang 
+    export_format = 'po'
+
+    
+with registry.cursor() as cr:
+    with open(filename, 'wb') as f:
+        trans_export(export_lang, [args.module], f, export_format, cr)
+        print(f"File creato con successo: {filename}")
+            
+#registry = odoo.modules.registry.Registry.delete(args.db)
